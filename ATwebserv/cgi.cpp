bb5c70b0cd94631b9be7b0ccdbd138aa8a891f85
Arthur BENOIT <abenoit@e2r11p20.clusters.42paris.fr>
2021-11-22 12:30:42 +0100

    hello.php working

--- ATwebserv/cgi.cpp
+++ ATwebserv/cgi.cpp
@@ -3,13 +3,40 @@
 int	main(int ac, char **av, char **env)
 {
 	cgi_handler	cgi;
+	std::string	lol;
+	int			bfd[2];
+	int			fd[2];
 	pid_t		pid;
 	char		**plop = cgi.args.data();//{ (char*)"files/cgi/php-cgi", NULL };
+	std::ofstream		out("out.html");
 
+	bfd[0] = dup(STDIN_FILENO);
+	bfd[1] = dup(STDOUT_FILENO);
+	if (pipe(fd) == -1)
+		return (CRASH_PIPE);
 	pid = fork();
+	if (pid == -1)
+		return (CRASH_FORK);
 	if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
 		execve(cgi.handler.c_str(), plop, NULL);
+		//close(STDOUT_FILENO);
+	}
 	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+//		out << (char*)HEADER;
+		while (getline(std::cin, lol))
+			out << lol << std::endl;
 		(waitpid(pid, NULL, 0));
+		dup2(bfd[0], STDIN_FILENO);
+		dup2(bfd[1], STDOUT_FILENO);
+//		out << (char*)FOOTER;
+	}
 	return (0);
 }
--- ATwebserv/cgi_handler.hpp
+++ ATwebserv/cgi_handler.hpp
@@ -1,15 +1,32 @@
 #ifndef CGI_HANDLER_HPP
 # define CGI_HANDLER_HPP
 
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-//#include <pthread.h>
-#include <sys/wait.h>
-#include <unistd.h>
+# include <iostream>
+# include <cstdlib>
+# include <vector>
+//# include <pthread.h>
+# include <sys/wait.h>
+# include <unistd.h>
+//# include <stringstram>
+# include <fstream>
+
+# define CRASH_FORK	-1
+# define CRASH_PIPE	-1
 
 # define CGI "files/cgi/php-cgi"
 # define SCRIPT "files/scripts/hello.php"
+
+# define HEADER	"\
+<!doctype html>\n\
+<html>\n\
+  <head>\n\
+    <title>This is the title of the webpage!</title>\n\
+  </head>\n\
+  	<body>\n"
+
+
+# define FOOTER "</body>\n\
+	</html>"
 
 class	cgi_handler	{
 //	private:
