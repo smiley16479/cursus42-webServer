bbda2bc2a2c333fa0c21b64e33404c634cfac3fa
Arthur BENOIT <abenoit@e2r11p20.clusters.42paris.fr>
2021-11-22 20:10:50 +0100

    small fixes and go_cgi added

--- ATwebserv/cgi.cpp
+++ ATwebserv/cgi.cpp
@@ -2,15 +2,20 @@
 
 int	main(int ac, char **av, char **env)
 {
-//	cgi_handler	cgi(&av[1]);
-	cgi_handler	cgi(av);
+	cgi_handler	cgi(&av[1]);
+//	cgi_handler	cgi(av);
 	std::string	lol;
 	int			bfd[2];
 	int			fd[2];
 	pid_t		pid;
-	char		**plop = cgi.args.data();//{ (char*)"files/cgi/php-cgi", NULL };
+	char		**plop = new char*[cgi.args.size() + 1];//{ (char*)"files/cgi/php-cgi", NULL };
+	int	i;
 	std::ofstream		out("out.html");
 
+	i = 0;
+	for (std::vector<std::string>::iterator it = cgi.args.begin(); it != cgi.args.end(); it++, i++)
+		plop[i] = (char*)it->c_str();
+	plop[i] = NULL;
 	bfd[0] = dup(STDIN_FILENO);
 	bfd[1] = dup(STDOUT_FILENO);
 	if (pipe(fd) == -1)
@@ -23,11 +28,13 @@ int	main(int ac, char **av, char **env)
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
-		execve(cgi.handler.c_str(), plop, NULL);
+		execve(plop[0], plop, NULL);
 		//close(STDOUT_FILENO);
 	}
 	else
 	{
+		for (int i = 0; plop[i]; i++)
+			printf("%s\n", plop[i]);
 		close(fd[1]);
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
@@ -39,5 +46,6 @@ int	main(int ac, char **av, char **env)
 		dup2(bfd[1], STDOUT_FILENO);
 //		out << (char*)FOOTER;
 	}
+	delete [] plop;
 	return (0);
 }
--- ATwebserv/cgi_handler.cpp
+++ ATwebserv/cgi_handler.cpp
@@ -1,5 +1,10 @@
 #include "cgi_handler.hpp"
 
+void	go_cgi(std::map<std::string, std::vector<std::string> mp)
+{
+
+}
+
 cgi_handler::cgi_handler()	{
 	handler = (char*)CGI;
 	args.push_back((char*)handler.c_str());
@@ -8,38 +13,49 @@ cgi_handler::cgi_handler()	{
 
 cgi_handler::cgi_handler(char **av)	{
 	int	pos;
-	std::string	query(av[1]);
-	std::string	tmp(query);
+	std::string	query((char*)av[0]);
+	std::string	path;
+	std::string	tmp;
 
 	handler = (char*)CGI;
 	args.push_back((char*)handler.c_str());
 //	args.push_back((char*)SCRIPT);
 
-/*
-	for (int i = 0; av[i] != NULL; i++)
+	if ((pos = query.find("?")) != std::string::npos)
 	{
-		query += (char*)av[i];
-		query += "&";
-	}
-	args.push_back((char*)query.c_str());
-	*/
-	if ((pos = query.find("?", 1)) > 0)
-	{
-		tmp = query.substr(0, pos);
-		args.push_back((char*)query.c_str());
-		tmp = tmp.substr(pos, std::string::npos);
-		while ((pos = tmp.find("&", 1)) > 0)
+		path = query.substr(0, pos);
+		args.push_back(path);
+		query = query.substr(pos + 1, std::string::npos);
+		while ((pos = query.find("&")) != std::string::npos)
 		{
-			tmp.substr(pos, std::string::npos);
-			args.push_back((char*)tmp.c_str());
+			tmp = query.substr(0, pos);
+			query = query.substr(pos + 1, std::string::npos);
+			args.push_back(tmp);
 		}
+		query = query.substr(pos + 1, std::string::npos);
+		args.push_back(query);
 	}
+	/*
+	else if ((pos = query.find(" ")) != std::string::npos)
+	{
+		path = query.substr(0, pos);
+		args.push_back(path);
+		query = query.substr(pos + 1, std::string::npos);
+		while ((pos = query.find(" ")) != std::string::npos)
+		{
+			tmp = query.substr(0, pos);
+			query = query.substr(pos + 1, std::string::npos);
+			args.push_back(tmp);
+		}
+		query = query.substr(pos + 1, std::string::npos);
+		args.push_back(query);
+	}
+	*/
 	else
 	{
 		for (int i = 0; av[i] != NULL; i++)
-			args.push_back(av[i]);
+			args.push_back((char*)av[i]);
 	}
-	args.push_back(NULL);
 }
 
 cgi_handler::cgi_handler(const cgi_handler& other)	{
